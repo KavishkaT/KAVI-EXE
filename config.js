b18f5c4ade6a86850abe2c3a30cd85fb4b707e83
KavishkaT <143253480+KavishkaT@users.noreply.github.com>
2024-12-11 00:05:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "tpR0VBpS#I16cGYvjUVA2B5nNblnukkwqsmVqLKAkuywVxxTMYQA",
+SESSION_ID: process.env.SESSION_ID || "Aog0RDQT#2Tq8iVBL41mcFwvTp8XAaJoiCoq6F6HS-FxMSSnKDYs",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/pkuujaup.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
